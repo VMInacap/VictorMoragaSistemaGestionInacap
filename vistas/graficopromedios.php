8b51de91383f365c9fc99486f0c37e6a57fc1722
VMInacap <v-moraga@hotmail.com>
2022-01-07 14:27:16 -0300

    cambios presentación grafico

--- vistas/graficopromedios.php
+++ vistas/graficopromedios.php
@@ -6,26 +6,34 @@
 			require_once ('../librerias/jpgraph/src/jpgraph_bar.php');
             $c = new conectar();
             $conexion = $c->conexion();
-			$sql1 = "select causas.id, causas.fecha_creacion, causas.fecha_termino, materias.materia from causas INNER JOIN materias ON materias.id=causas.id_materia";
+			$sql1 = "select causas.id, causas.fecha_creacion, causas.fecha_termino, materias.materia, causas.id_materia from causas INNER JOIN materias ON materias.id=causas.id_materia ORDER BY causas.id_materia DESC";
             $rs1 = mysqli_query($conexion, $sql1);	
 $datay=array();			
 $datax=array();	
-while($tabla=  mysqli_fetch_row($rs1)):
+$suma=array();
+while($tabla=  mysqli_fetch_row($rs1)){
 $tabla[1];
 $tabla[2];
 $date1=$tabla[1];
 $date2=$tabla[2];
 $materia=$tabla[3];	
+$i=1;
+$idmateria=$tabla[4];
+$sql2 = "select count(id_materia) as materiaa from causas where id_materia='$idmateria'";
+$rs2 = mysqli_query($conexion, $sql2);
+while($mat=mysqli_fetch_row($rs2)){
 $date1_ts=strtotime($date1);
 $date2_ts=strtotime($date2);
 $diff=$date2_ts - $date1_ts;
 $datediff=round($diff/86400);
 $datay[]=$datediff;
 $datax[]=$materia;
-endwhile;
+
+}
+}
 
 // Create the graph. These two calls are always required
-$graph = new Graph(700,400);
+$graph = new Graph(1400,600);
 $graph->SetScale('textlin');
  
 // Add a drop shadow
@@ -40,16 +48,19 @@ $bplot = new BarPlot($datay);
 // Adjust fill color
 $bplot->SetFillColor('orange');
 $graph->Add($bplot);
- 
+$bplot->value->Show();
+$bplot->value->SetFont(FF_ARIAL,FS_BOLD);
+$bplot->value->SetFormat('%01.0f dias'); 
 // Setup the titles
 $graph->title->Set('Tiempo de causas');
 $graph->xaxis->title->Set('Materias de causas');
 $graph->yaxis->title->Set('Dias');
 $graph->xaxis->SetTickLabels($datax);
+$graph->xaxis->SetFont(FF_ARIAL,FS_NORMAL,8);
  
-$graph->title->SetFont(FF_FONT1,FS_BOLD);
-$graph->yaxis->title->SetFont(FF_FONT1,FS_BOLD);
-$graph->xaxis->title->SetFont(FF_FONT1,FS_BOLD);
+$graph->title->SetFont(FF_ARIAL,FS_BOLD);
+$graph->yaxis->title->SetFont(FF_ARIAL,FS_BOLD);
+$graph->xaxis->title->SetFont(FF_ARIAL,FS_BOLD);
  
 // Display the graph
 $graph->Stroke();
